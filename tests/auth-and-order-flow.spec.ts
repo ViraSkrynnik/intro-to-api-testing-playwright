f7a8e3ff6524f4706f925aeddff4bb184d155287
Vira Skrynnik <veronskr@gmail.com>
2025-06-25 20:03:24 +0300

    auth-and-order-flow.spec.ts: - improved existed tests - added new tests

--- tests/auth-and-order-flow.spec.ts
+++ tests/auth-and-order-flow.spec.ts
@@ -7,36 +7,68 @@ const serviceURL = 'https://backend.tallinn-learning.ee/'
 const loginPath = 'login/student'
 const orderPath = 'orders'
 
-// JWT pattern in the form of a regular expression
-const jwtPattern = /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
-
 test.describe('Tallinn delivery API tests', () => {
   test('login with correct data and verify auth token', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithCorrectData()
-    console.log('requestBody:', requestBody)
     const response = await request.post(`${serviceURL}${loginPath}`, {
       data: requestBody,
     })
-    const responseBody = await response.text()
 
-    console.log('response code:', response.status())
-    console.log('response body:', responseBody)
-    expect(response.status()).toBe(StatusCodes.OK)
-    expect(jwtPattern.test(responseBody)).toBeTruthy()
+    const jwtValue = await response.text()
+    const jwtRegex = /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
+
+    expect.soft(response.status()).toBe(StatusCodes.OK)
+    expect.soft(jwtRegex.test(jwtValue)).toBeTruthy()
   })
 
   test('login with incorrect data and verify response code 401', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithIncorrectData()
-    console.log('requestBody:', requestBody)
     const response = await request.post(`${serviceURL}${loginPath}`, {
       data: requestBody,
     })
     const responseBody = await response.text()
 
-    console.log('response code:', response.status())
-    console.log('response body:', responseBody)
     expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
     expect(responseBody).toBe('')
+  })
+
+  test('login with correct data and incorrect HTTP method PUT', async ({ request }) => {
+    const requestBody = LoginDto.createLoginWithCorrectData()
+    const response = await request.put(`${serviceURL}${loginPath}`, {
+      data: requestBody,
+    })
+
+    const responseBody = await response.json()
+
+    expect.soft(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+    expect.soft(responseBody.path).toBe(`/${loginPath}`)
+  })
+
+  test('login with incorrect HTTP method GET', async ({ request }) => {
+    const requestBody = LoginDto.createLoginWithCorrectData()
+    const response = await request.get(`${serviceURL}${loginPath}`, {
+      data: requestBody,
+    })
+
+    const responseBody = await response.json()
+
+    expect.soft(responseBody.path).toBe(`/${loginPath}`)
+    expect.soft(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+  })
+
+  test('login with invalid body structure 400', async ({ request }) => {
+    const invalidRequestBody = {
+      username: 123,
+      password: 456,
+    }
+    const response = await request.post(`${serviceURL}${loginPath}`, {
+      data: invalidRequestBody,
+    })
+
+    const responseBody = await response.json()
+
+    expect.soft(response.status()).toBe(StatusCodes.UNAUTHORIZED)
+    expect.soft(responseBody.path).toBe(`/${loginPath}`)
   })
 
   test('login and create order', async ({ request }) => {
@@ -44,17 +76,18 @@ test.describe('Tallinn delivery API tests', () => {
     const response = await request.post(`${serviceURL}${loginPath}`, {
       data: requestBody,
     })
-    const jwt = await response.text()
+    const jwtValue = await response.text()
     const orderResponse = await request.post(`${serviceURL}${orderPath}`, {
       data: OrderDto.createOrderWithRandomData(),
       headers: {
-        Authorization: `Bearer ${jwt}`,
+        Authorization: `Bearer ${jwtValue}`,
       },
     })
 
     const orderResponseBody = await orderResponse.json()
-    console.log('orderResponse status:', orderResponse.status())
-    console.log('orderResponse:', orderResponseBody)
+    const jwtRegex = /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
+
+    expect.soft(jwtRegex.test(jwtValue)).toBeTruthy()
     expect.soft(orderResponse.status()).toBe(StatusCodes.OK)
     expect.soft(orderResponseBody.status).toBe('OPEN')
     expect.soft(orderResponseBody.id).toBeDefined()
