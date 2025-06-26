18f54e0dfffe72b7f82ae5ebbfae3ac2402de286
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2025-06-26 13:04:23 +0300

    Fix requests
    # Conflicts:
    #	tests/auth-tests-with-api-client.spec.ts

--- tests/auth-tests-with-api-client.spec.ts
+++ tests/auth-tests-with-api-client.spec.ts
@@ -4,14 +4,14 @@ import { StatusCodes } from 'http-status-codes'
 
 test('login and create order with api client', async ({ request }) => {
   const apiClient = await ApiClient.getInstance(request)
-  const orderId = await apiClient.createOrderAndReturnOrderId()
+  const orderId = await apiClient.createOrderAndReturnOrderId(request)
   console.log('orderId:', orderId)
 })
 
 test('Successful login and delete existed order with api client', async ({ request }) => {
   const apiClient = await ApiClient.getInstance(request)
-  const orderId = await apiClient.createOrderAndReturnOrderId()
-  const response = await apiClient.deleteOrder(orderId)
+  const orderId = await apiClient.createOrderAndReturnOrderId(request)
+  const response = await apiClient.deleteOrder(orderId, request)
   const responseBody = await response.ok()
   expect.soft(response.status()).toBe(StatusCodes.OK)
   expect.soft(responseBody).toBeTruthy()
